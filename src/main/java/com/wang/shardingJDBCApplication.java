309e760cefe4e996cc71b3c995cea18889e8d6c0
Your Name <you@example.com>
2023-05-19 21:20:36 +0800

    shardingJDBC 读写分离

---
+++ src/main/java/com/wang/shardingJDBCApplication.java
@@ -0,0 +1,14 @@
+package com.wang;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class shardingJDBCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(shardingJDBCApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
